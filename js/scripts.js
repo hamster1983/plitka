db0f4ab4c02ed047c6237f39a78331bc2422798a
humster_spb <you@example.com>
2020-03-16 23:35:33 +0400

    version 16.03 start

---
+++ js/scripts.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+	
+	$('[name="phone"]').inputmask("+7(999) 999-9999");
+	
+	$('[type="submit"]').click(function(e) {
+		let form = $(this).parents('form');
+		if(form.find('[name="phone"]').val() == '' || form.find('[name="name"]').val() == '') {
+			alert('Поля ИМЯ и ТЕЛЕФОН не могут быть пустыми! Заявка НЕ отправлена! Попробуйте еще раз');
+			form.find('[type="text"]')[0].focus();
+			return false;
+		}
+		e.preventDefault();
+		$.ajax({
+		  cashe: false,
+		  type: form.attr('method'),
+		  url: form.attr('action'),
+		  data: form.serialize(),
+		  success: function(data) {
+			$('#result .modal-body').html(data);
+			$('#result').modal('show');
+			//$('form [type="text"]').val('');
+		  }
+		});
+	});
+	
+	$('.top-slider').slick({
+	  infinite: true,
+	  autoplay: true,
+	  autoplaySpeed: 1500,
+	  arrows: false,
+	  dots: false,
+	  speed: 1500,
+	  slidesToShow: 5,
+	  slidesToScroll: 1,
+	  responsive: [
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 4,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 3,
+		  }
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 630,
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}
+	  ] 
+	});
+	
+	$('.bottom-slider').slick({
+	  infinite: true,
+	  autoplay: true,
+	  autoplaySpeed: 1500,
+	  arrows: false,
+	  dots: false,
+	  speed: 1500,
+	  slidesToShow: 5,
+	  slidesToScroll: 1,
+	  responsive: [
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 4,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 3,
+		  }
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 630,
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}
+	  ] 
+	});
+	
+});
