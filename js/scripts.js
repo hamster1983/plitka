288c86e9bef5b240e3b7bb25e1f0af8885c84518
humster_spb <you@example.com>
2020-03-18 00:44:52 +0400

    version 18.03

--- js/scripts.js
+++ js/scripts.js
@@ -26,38 +26,13 @@ $(document).ready(function(){
 	$('.top-slider').slick({
 	  infinite: true,
 	  autoplay: true,
-	  autoplaySpeed: 1500,
+	  fade: true,
+	  autoplaySpeed: 2000,
 	  arrows: false,
 	  dots: false,
-	  speed: 1500,
-	  slidesToShow: 5,
-	  slidesToScroll: 1,
-	  responsive: [
-		{
-		  breakpoint: 1199,
-		  settings: {
-			slidesToShow: 4,
-		  }
-		},
-		{
-		  breakpoint: 991,
-		  settings: {
-			slidesToShow: 3,
-		  }
-		},
-		{
-		  breakpoint: 767,
-		  settings: {
-			slidesToShow: 2,
-		  }
-		},
-		{
-		  breakpoint: 630,
-		  settings: {
-			slidesToShow: 1,
-		  }
-		}
-	  ] 
+	  speed: 2000,
+	  slidesToShow: 1,
+	  slidesToScroll: 1
 	});
 	
 	$('.bottom-slider').slick({
